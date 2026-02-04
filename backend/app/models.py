02071f5385716b8479199437a0d99c1e03eec5bc
itsGab <gabriel.schultz10@gmail.com>
2026-02-04 18:35:03 -0300

    feat(user): adiciona create e update timestamps
    - user: adiciona timestamps
    - alembic: adiciona campos ao banco de dados
    - env: adiciona render_as_batch para alteracoes no sqlite

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,19 @@
-from sqlmodel import Field, SQLModel
+from datetime import datetime
+
+from sqlmodel import Column, DateTime, Field, SQLModel, func
+
+
+class TimestampModel(SQLModel):
+    # campos para horario da criacao e atualizacao
+    created_at: datetime = Field(
+        default=None, sa_column=Column(DateTime, server_default=func.now())
+    )
+    updated_at: datetime = Field(
+        default=None,
+        sa_column=Column(
+            DateTime, server_default=func.now(), onupdate=func.now()
+        ),
+    )
 
 
 class UserInput(SQLModel):
@@ -6,7 +21,7 @@ class UserInput(SQLModel):
     password: str
 
 
-class User(SQLModel, table=True):
+class User(TimestampModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     username: str
     password: str
